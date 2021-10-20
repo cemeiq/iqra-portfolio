8b8135ff310b7ee2aba53431ea3f990b76614c30
iqra <iqra.muhammad@liverpool.ac.uk>
2021-10-20 16:42:19 +0100

    add about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Intro from './components/intro/Intro'
-
+import About from './components/About/About'
 function App() {
   return (
-    <div><Intro/></div>
+    <div><Intro/>
+    <About/></div>
+    
   );
 }
 
---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className="about">
+           <div className="about-left">
+               <div className="about-card-bg">
+                   
+                   
+               </div>
+               <div className="about-card">
+               <img className="about-image" src="https://www.blog.motifphotos.com/wp-content/uploads/2018/12/iStock-902506410_1200x800px-1200x800.jpg"></img>
+               </div>
+           </div>
+           <div className="about-right"></div>
+        </div>
+    )
+}
+
+export default About
