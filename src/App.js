d12ed212686f4f6299a9b2924e8c3657cdf0d128
iqra <iqra.muhammad@liverpool.ac.uk>
2021-10-29 11:55:47 +0100

    feature: add product and productlist

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Intro from './components/intro/Intro'
 import About from './components/About/About'
+import ProductList from './components/productList/ProductList';
 function App() {
   return (
-    <div><Intro/>
-    <About/></div>
+    <div>
+    <Intro/>
+    <About/>
+    <ProductList/>
+    </div>
     
   );
 }
--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import './productlist.css'
+import Product from '../Product/Product'
 function ProductList() {
     return (
-        <div>
-            ProductList
+        <div className="productlist">
+            <div className="productlist-texts">
+            <h1 className="productlist-title">Create & inspire. It's Lama</h1>
+            <p className="productlist-desc">
+          Lama is a creative portfolio that your work has been waiting for.
+          Beautiful homes, stunning portfolio styles & a whole lot more awaits
+          inside.
+        </p>
+            </div>
+
+           <div className="productlist-list">
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+            </div> 
         </div>
     )
 }
